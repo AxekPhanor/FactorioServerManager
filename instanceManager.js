bae7166b53753c46a00514f0b778e2df963939f7
Axek <axel.phanor64@gmail.com>
2025-08-11 16:52:09 +0200

    first commit

---
+++ instanceManager.js
@@ -0,0 +1,53 @@
+var ovh = require('@ovhcloud/node-ovh')({
+    endpoint: 'ovh-eu',
+    appKey: 'f0ef7d770e596fa4',
+    appSecret: '8a8b877f2bf049eb4fcfca038f29eaf7',
+    consumerKey: '5c128bc81f0d51e38ae0eaeaee0a1c5b'
+});
+
+async function instance() {
+    return new Promise((resolve, reject) => {
+        ovh.request('GET', '/cloud/project/8b5103a48edb4a28998cff632f9b9f12/instance', (err, result) => {
+            if (err) return reject(err);
+            resolve(result);
+        });
+    });
+}
+
+async function start() {
+    await ovh.request('POST', '/cloud/project/8b5103a48edb4a28998cff632f9b9f12/instance/bcdd4963-14ec-43cf-b08c-e9c2759303ee/start', function (err, result ) {
+        if(err) {
+            console.error(err);
+        }
+        else {
+            console.log("Démarrage du serveur...");
+            console.log(result);
+        }
+    });
+}
+
+async function stop() {
+    await ovh.request('POST', '/cloud/project/8b5103a48edb4a28998cff632f9b9f12/instance/bcdd4963-14ec-43cf-b08c-e9c2759303ee/stop', function (err, result ) {
+        if(err) {
+            console.error(err);
+        }
+        else {
+            console.log("Fermeture du serveur...");
+            console.log(result);
+        }
+    });
+}
+
+async function reboot() {
+    await ovh.request('POST', '/cloud/project/8b5103a48edb4a28998cff632f9b9f12/instancebcdd4963-14ec-43cf-b08c-e9c2759303ee/reboot', function (err, result ) {
+        if(err) {
+            console.error(err);
+        }
+        else {
+            console.log("Relance du serveur...");
+            console.log(result);
+        }
+    });
+}
+
+module.exports = { instance, start, stop, reboot };
---
+++ main.js
@@ -0,0 +1,100 @@
+express = require('express');
+require('dotenv/config');
+require('discord-interactions');
+instanceManager = require('./instanceManager.js');
+utils = require('./utils.js')
+const { verifyKeyMiddleware, InteractionType, InteractionResponseType } = require('discord-interactions');
+
+const app = express();
+const port = 3000;
+app.listen(port, () => {
+    console.log('Listening on port', port);
+});
+
+const instanceCommand = {
+    name: 'instance',
+    description: 'Instance command',
+    type: 1,
+};
+
+const startCommmand = {
+    name: 'start',
+    description: 'Start command',
+    type: 1,
+};
+
+const stopCommand = {
+    name: 'stop',
+    description: 'Stop command',
+    type: 1,
+};
+
+const rebootCommand = {
+    name: 'reboot',
+    description: 'Reboot command',
+    type: 1,
+};
+
+utils.InstallGlobalCommands(process.env.APP_ID, [instanceCommand, startCommmand, stopCommand, rebootCommand]);
+
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
+    const { type, data } = req.body;
+    console.log("Interaction reçue");
+
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data;
+
+        if (name === 'instance') {
+            try {
+                const content = await instanceManager.instance();
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: '```json\n' + JSON.stringify(content, null, 2) + '\n```'
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: "Erreur lors de la récupération de l'instance."
+                    }
+                });
+            }
+        }
+        if (name === 'start') {
+            await instanceManager.start();
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: "Démarrage du serveur..."
+                }
+            });
+        }
+        if (name === 'stop') {
+            await instanceManager.stop();
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: "Fermeture du serveur..."
+                }
+            });
+        }
+        if (name === 'reboot') {
+            await instanceManager.reboot();
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    content: "Relance du serveur..."
+                }
+            });
+        }
+
+        console.error(`unknown command: ${name}`);
+        return res.status(400).json({ error: 'unknown command' });
+    }
+
+    console.error('unknown interaction type', type);
+    return res.status(400).json({ error: 'unknown interaction type' });
+});
